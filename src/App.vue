848e4fc94291f291394e22f995bc559a54ec2da3
Melissa Pula <melissa_m_24@yahoo.com>
2021-07-31 14:57:27 -0500

    add to public index

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Does this make any changes?"/>
   </div>
 </template>
 
