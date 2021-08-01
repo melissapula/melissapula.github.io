76631366ea59c152847e6a3d7a0c4a9d5dc9078a
Melissa Pula <melissa_m_24@yahoo.com>
2021-08-01 10:20:09 -0500

    test

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,42 @@
 <template>
   <mdb-container>
-    <mdb-row>
-      <mdb-col col="sm">One of three columns</mdb-col>
-      <mdb-col col="sm">One of three columns</mdb-col>
-      <mdb-col col="sm">One of three columns</mdb-col>
-    </mdb-row>
-  </mdb-container>
-  <!--<mdb-row>
-    <mdb-col col="4" v-if="showTheForce == false" class="img-fluid">
-      <img :src="theForceBefore">
-        <mdb-btn type="button" class="btn mt-2" style="background-color: #1F8AC0; color: #104C91" @click="showTheForce = true"><b>Use the Force</b></mdb-btn>
+  <mdb-row class="mt-4 align-middle justify-content-center">
+    <mdb-col col="5" v-if="showTheForce == false">
+      <mdb-row>
+        <mdb-col col="12">
+      <img :src="theForceBefore" class="img-fluid">
+        </mdb-col>
+      </mdb-row>
+      <mdb-row class="justify-content-center">
+        <mdb-btn type="button" class="btn mt-2 mx-auto" style="background-color: #1F8AC0; color: #104C91" @click="showTheForce = true"><b>Use the Force</b></mdb-btn>
+      </mdb-row>
     </mdb-col>
-    <mdb-col col="4" v-if="showTheForce == true" class="img-fluid">
-      <img :src="theForceAfter">
+    <mdb-col col="5" v-if="showTheForce == true">
+      <img :src="theForceAfter" class="img-fluid">
         <mdb-btn type="button" class="btn mt-2" style="background-color: #1F8AC0; color: #104C91" @click="showTheForce = false"><b>End the Force</b></mdb-btn>
     </mdb-col>
-    <mdb-col col="4">
+    <mdb-col col="5">
         <mdb-row>
           <mdb-col col="12">
-          <h1 class="text-center" style="color:#104C91"><b>M E L I S S A   P U L A</b></h1>
+            <h1 class="text-center" style="color:#104C91"><span class="mr-3"><b>M E L I S S A</b></span><span class="ml-3"><b>P U L A</b></span></h1>
           <h5 class="text-center" style="color:#104C91">Mother. Wife. Nerd.</h5>
           </mdb-col>
         </mdb-row>
       <mdb-row class="justify-content-center">
-        <mdb-col col="12" class="mx-auto">
-          <a href="aboutPage.html" type="button" class="btn" style="background-color:#1F8AC0; color:#104C91"><b>About</b><i class="fa-fw bi bi-file-person"></i></a>
+          <mdb-btn-group vertical>
+            <mdb-btn style="background-color:#1F8AC0; color:#104C91" class="mb-2">About<mdb-icon icon="profile" class="ml-2"></mdb-icon></mdb-btn>
+            <mdb-btn style="background-color:#1F8AC0; color:#104C91" class="mb-2">Resume<mdb-icon icon="file-earmark-text" class="ml-2"></mdb-icon></mdb-btn>
+            <mdb-btn style="background-color:#1F8AC0; color:#104C91" class="mb-2">Example Work<mdb-icon icon="laptop" class="ml-2"></mdb-icon></mdb-btn>
+            <mdb-btn style="background-color:#1F8AC0; color:#104C91" class="mb-2">Blog<mdb-icon icon="pencil" class="ml-2"></mdb-icon></mdb-btn>
+          </mdb-btn-group>
+          <!--<a href="aboutPage.html" type="button" class="btn" style="background-color:#1F8AC0; color:#104C91"><b>About</b><i class="fa-fw bi bi-file-person"></i></a>
           <a href="resumePage.html" type="button" class="btn" style="background-color:#1F8AC0; color:#104C91"><b>Resume</b><i class="bi bi-file-earmark-text"></i></a>
           <a href="fishCreek.html" type="button" class="btn" style="background-color:#1F8AC0; color:#104C91"><b>Example Work</b><i class="bi bi-laptop"></i></a>
-          <a href="blogPage.html" type="button" class="btn" style="background-color:#1F8AC0; color:#104C91"><b>Blog</b><i class="bi bi-pencil"></i></a>
-      </mdb-col>
+          <a href="blogPage.html" type="button" class="btn" style="background-color:#1F8AC0; color:#104C91"><b>Blog</b><i class="bi bi-pencil"></i></a>-->
       </mdb-row>
     </mdb-col>
-  </mdb-row>-->
+  </mdb-row>
+  </mdb-container>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
 import App from './App.vue'
+import * as mdbvue from "mdbvue";
 
 Vue.config.productionTip = false
+
+for (const component in mdbvue) {
+  Vue.component(component, mdbvue[component]);
+}
 
 new Vue({
   render: h => h(App),
