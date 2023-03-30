e483a961dafd1d9c66f005720f9afebba9f72690
Melissa Pula <melissa.pula@transimpact.com>
2023-03-30 12:22:57 -0500

    add navbar and home page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <mdb-container class="h-100 m-0 p-0" fluid>
+    <mdb-navbar color="default" class="white-text" dark>
+      <mdb-navbar-brand href="https://mdbootstrap.com/"><router-link style="color: white" to="/">Melissa Pula</router-link>
+      </mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item active><router-link to="/" class="mr-3 white-text" active>Home<mdb-icon icon="home" class="ml-2"></mdb-icon></router-link></mdb-nav-item>
+        </mdb-navbar-nav>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+    <router-view/>
+  </mdb-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import * as mdbvue from "mdbvue";
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+for (const component in mdbvue) {
+  Vue.component(component, mdbvue[component]);
+}
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
