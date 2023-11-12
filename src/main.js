805bbe31371e67ad2ee68878509aee4e204960ad
Melissa Pula <melissa.pula@transimpact.com>
2023-11-12 14:00:24 -0600

    feat: add google analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as mdbvue from "mdbvue";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbvue/lib/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import VueGtag from 'vue-gtag';
 import router from "./router";
 import Vue from 'vue';
 import App from './App.vue'
@@ -10,7 +11,16 @@ for (const component in mdbvue) {
   Vue.component(component, mdbvue[component]);
 }
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: {
+    id: 'G-ZP2LCLVZ2X',
+    params: {
+      send_page_view: false
+    }
+  }
+}, router)
 
 new Vue({
   router,
